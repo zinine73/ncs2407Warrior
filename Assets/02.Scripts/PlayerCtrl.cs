4d69c53b42e50c6fadc4a37093924ed826a53218
zinine73 <zinine@naver.com>
2024-10-11 12:19:05 +0900

    input system

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -9,10 +9,37 @@ public class PlayerCtrl : MonoBehaviour
     private new Transform transform;
     private Vector3 moveDir;
 
+    private PlayerInput playerInput;
+    private InputActionMap mainActionMap;
+    private InputAction moveAction;
+    private InputAction attackAction;
+
     void Start()
     {
         anim = GetComponent<Animator>();
         transform = GetComponent<Transform>();
+        playerInput = GetComponent<PlayerInput>();
+
+        mainActionMap = playerInput.actions.FindActionMap("PlayerActions");
+
+        moveAction = mainActionMap.FindAction("Move");
+        attackAction = mainActionMap.FindAction("Attack");
+
+        moveAction.performed += ctx => {
+            Vector2 dir = ctx.ReadValue<Vector2>();
+            moveDir = new Vector3(dir.x, 0, dir.y);
+            anim.SetFloat("Movement", dir.magnitude);
+        };
+
+        moveAction.canceled += ctx => {
+            moveDir = Vector3.zero;
+            anim.SetFloat("Movement", 0.0f);
+        };
+
+        attackAction.performed += ctx => {
+            Debug.Log("Attack by c# event");
+            anim.SetTrigger("Attack");
+        };
     }
 
     void Update()
@@ -24,6 +51,7 @@ public class PlayerCtrl : MonoBehaviour
         }
     }
 
+#region SEND_MESSAGE
     void OnMove(InputValue value)
     {
         Vector2 dir = value.Get<Vector2>();
@@ -37,4 +65,24 @@ public class PlayerCtrl : MonoBehaviour
         anim.SetTrigger("Attack");
         Debug.Log("Attack");
     }
+#endregion SEND_MESSAGE
+
+#region UNITY_EVENTS
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        Vector2 dir = ctx.ReadValue<Vector2>();
+        moveDir = new Vector3(dir.x, 0, dir.y);
+        anim.SetFloat("Movement", dir.magnitude);
+    }
+
+    public void OnAttack(InputAction.CallbackContext ctx)
+    {
+        Debug.Log($"ctx.phase={ctx.phase}");
+        if (ctx.performed)
+        {
+            Debug.Log("Attack");
+            anim.SetTrigger("Attack");
+        }
+    }
+#endregion UNITY_EVENTS
 }
